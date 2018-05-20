cec009c4ffef2ab2a5f2f9387c2c50f9192d5eff
bind-almir <almir@bind.ba>
2018-05-20 23:41:52 +0200

    bootstrap date, moment

--- src/app/components/project-form/project-form.component.ts
+++ src/app/components/project-form/project-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../../services/app.service';
 import { NgbCalendar, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-project-form',
@@ -17,6 +18,8 @@ export class ProjectFormComponent implements OnInit {
 
   ngOnInit() {
     if(this.app.selectedProject) {
+      this.toDate = this.dateToStruct(this.app.selectedProject.ProjectEndDate);
+      this.fromDate = this.dateToStruct(this.app.selectedProject.ProjectStartDate);
       this.isUpdate = true;
     } else {
       this.app.selectedProject = {
@@ -28,7 +31,22 @@ export class ProjectFormComponent implements OnInit {
     }
   }
 
+  dateToStruct(date): NgbDateStruct {
+    date = new Date(date);
+    return {
+      year: date.getFullYear(),
+      month: date.getMonth()+1,
+      day: date.getDate()
+    }
+  }
+
   saveProject() {
+    if(this.fromDate) {
+      this.app.selectedProject.ProjectStartDate = new Date(this.fromDate.year, this.fromDate.month, this.fromDate.day);
+    }
+    if(this.toDate) {
+      this.app.selectedProject.ProjectEndDate = new Date(this.toDate.year, this.toDate.month, this.toDate.day);
+    }
     if(this.isUpdate) {
       this.app.updateProject(this.app.selectedProject).subscribe((result: any) => {
         if(result.statusCode == 200) {
