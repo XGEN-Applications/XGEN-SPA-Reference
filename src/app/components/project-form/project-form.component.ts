7dca8a2737b7193376c0997a31889165e5dffac9
almir@bind.ba <almir@bind.ba>
2018-05-21 15:21:00 +0200

    update project

--- src/app/components/project-form/project-form.component.ts
+++ src/app/components/project-form/project-form.component.ts
@@ -49,7 +49,6 @@ export class ProjectFormComponent implements OnInit {
     // Goal ID = 2
     // Plan ID = 2
     const { id } = this.app.getUserId(); 
-    console.log(id)
     //Logged user Id, this should be done on server side, this is just a reference
     this.app.selectedProject.ObjectiveID = 2;
 
@@ -64,40 +63,53 @@ export class ProjectFormComponent implements OnInit {
       this.app.selectedProject.ProjectEndDate = this.app.parseDate(date);
     }
 
+    if(this.app.selectedProject.CreateDate) {
+      const date = moment(new Date(this.app.selectedProject.CreateDate)).format('YYYY-MM-DD');
+      this.app.selectedProject.CreateDate = this.app.parseDate(date);
+    }
+
+    if(this.app.selectedProject.UpdateDate) {
+      const date = moment(new Date(this.app.selectedProject.UpdateDate)).format('YYYY-MM-DD');
+      this.app.selectedProject.UpdateDate = this.app.parseDate(date);
+    }
+
     if(this.isUpdate) { 
-
-      this.app.selectedProject.UpdateBy = id;
-
-      this.app.updateProject(this.app.selectedProject).subscribe((result: any) => {
-      
-        if(result.statusCode == 200) {
-          this.app.messageAlert.emit({ alert: 'info', message: 'project updated' });
-        } else {
-          this.app.messageAlert.emit({ alert: 'error', message: JSON.parse(result.body) });
-        }
-
-      }, err => {
-        this.app.messageAlert.emit({ alert: 'error', message: 'server error' })
-      });
-
+      this.updateProject(id);
     } else {
-
-      this.app.selectedProject.CreateBy = id;
-
-      this.app.addProject(this.app.selectedProject).subscribe((result: any) => {
-        if(result.statusCode == 200) {
-          this.app.messageAlert.emit({ alert: 'info', message: 'project inserted' });
-        } else {
-          this.app.messageAlert.emit({ alert: 'error', message: JSON.parse(result.body) });
-        }
-
-      }, err => {
-        this.app.messageAlert.emit({ alert: 'error', message: 'server error' })
-      });
-
+      this.createProject();
     }
     this.app.navigate('home');
   
+  }
+
+  updateProject(id) {
+    this.app.selectedProject.UpdateBy = id;
+    console.log(this.app.selectedProject);
+    this.app.updateProject(this.app.selectedProject).subscribe((result: any) => {
+    
+      if(result.statusCode == 200) {
+        this.app.messageAlert.emit({ alert: 'info', message: 'project updated' });
+      } else {
+        this.app.messageAlert.emit({ alert: 'error', message: JSON.parse(result.body) });
+      }
+
+    }, err => {
+      this.app.messageAlert.emit({ alert: 'error', message: 'server error' })
+    });
+  }
+
+  createProject() {
+
+    this.app.addProject(this.app.selectedProject).subscribe((result: any) => {
+      if(result.statusCode == 200) {
+        this.app.messageAlert.emit({ alert: 'info', message: 'project inserted' });
+      } else {
+        this.app.messageAlert.emit({ alert: 'error', message: JSON.parse(result.body) });
+      }
+
+    }, err => {
+      this.app.messageAlert.emit({ alert: 'error', message: 'server error' })
+    });
   }
 
 }
